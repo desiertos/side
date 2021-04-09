d0d7a6928aabb38939ec99275daddfe1dec1c6d4
tiagombp <tiagombp@gmail.com>
2021-04-08 22:36:40 -0300

    gera máscara argentina e geojson das provincias

--- geo.R
+++ geo.R
@@ -1,13 +1,11 @@
-library(sf)
 library(tidyverse)
+library(sf)
+library(geojsonsf)
 
-arg <- read_sf(dsn = "./geo_data/provincia", layer = "provincia")
+arg <- read_sf(dsn = "./provincia", layer = "provincia")
+arg_pais <- read_sf(dsn = "./pais", layer = "pais")
 
-arg_simples <- sf::st_simplify(arg)
-
-arg_continental <- st_filter(x <= -53, y >= -56)
-
-sf::st_crs(arg_simples)
+arg_simples <- sf::st_simplify(arg_pais)
 
 bbox_arg_continental <- data.frame(
   lon = c(-53, -53, -75, -75),
@@ -19,8 +17,25 @@ polygon <- bbox_arg_continental %>%
   summarise(geometry = st_combine(geometry)) %>%
   st_cast("POLYGON")
 
-ggplot(polygon) + geom_sf()
+arg_cont <- st_intersection(polygon, arg_simples)
 
-arg_continental <- st_intersection(polygon, arg_simples)
+ggplot(arg_cont) + geom_sf()
 
-ggplot(arg_continental) + geom_sf()
+#mascara
+
+world <- geojson_sf("world.geojson")
+
+st_crs(world) <- st_crs(arg_cont)
+
+world_crs <- st_transform(world, st_crs(world))
+
+arg_mask <- sf::st_difference(world_crs, arg_cont)
+
+ggplot(arg_mask) + 
+  geom_sf()
+
+arg_geojson <- geojsonsf::sf_geojson(arg)
+write_file(arg_geojson, "arg.geojson")
+
+arg_mask_geojson <- geojsonsf::sf_geojson(arg_mask)
+write_file(arg_mask_geojson, "arg_mask.geojson")
