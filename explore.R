776bafdcf893c1420b6c1239b0f8a46b52aaabeb
tiagombp <tiagombp@gmail.com>
2021-04-21 21:35:21 -0300

    prototyping charts

--- explore.R
+++ explore.R
@@ -25,13 +25,13 @@ dados_consolidados <- datos %>% bind_rows()
 
 dados_sumarizados <- dados_consolidados %>%
   group_by(provincia) %>%
-  mutate(media_provincia = mean(`Relación Población residente/medios`, na.rm = TRUE)) %>%
+  mutate(media_provincia = ifelse(row_number() == 1, mean(`Relación Población residente/medios`, na.rm = TRUE), NA)) %>%
   ungroup()
 
 ggplot(dados_sumarizados, aes(y = provincia, x = `Relación Población residente/medios`)) +
-  geom_quasirandom(groupOnX = FALSE, aes(color = `Categoría`)) +
-  geom_tile(aes(x = media_provincia), height = .75, width = 20) +
-  geom_vline(xintercept = mean(dados_sumarizados$`Relación Población residente/medios`, na.rm = TRUE), size = .5, linetype = "dotted") +
+  geom_tile(aes(x = media_provincia), width = 20) +
+  geom_vline(xintercept = mean(dados_sumarizados$`Relación Población residente/medios`, na.rm = TRUE), size = .5, linetype = "dotted", color = "gray") +
+  geom_quasirandom(groupOnX = FALSE, aes(color = `Categoría`), size = 2) +
   scale_x_continuous(limits = c(NA, 40e3), labels = function(x){format(x, big.mark = ".")}) +
   scale_color_manual(values = cores, labels = nomes) +
   labs(y = NULL, color = NULL) +
@@ -44,4 +44,29 @@ ggplot(dados_sumarizados, aes(y = provincia, x = `Relación Población residente
         axis.text.x = element_text(size = 8),
         legend.position = "top")
 
-ggsave("beeswarm.png", plot=last_plot(), width = 6, height = 4.5)
+ggsave("./plots/beeswarm.png", plot=last_plot(), width = 6, height = 4.5)
+
+salta <- dados_sumarizados %>% 
+  filter(provincia == "Salta") %>%
+  mutate(
+    media_provincia = mean(`Relación Población residente/medios`, na.rm = TRUE),
+    media_nacional  = mean(dados_sumarizados$`Relación Población residente/medios`))
+    
+
+
+ggplot(salta, aes(y = Departamento, x = `Relación Población residente/medios`)) +
+  geom_col(fill = "lavender") + 
+  geom_vline(xintercept = mean(salta$`Relación Población residente/medios`, na.rm = TRUE), size = .5, linetype = "dotted", color = "purple") +
+  geom_vline(xintercept = mean(dados_sumarizados$`Relación Población residente/medios`, na.rm = TRUE), size = .5, linetype = "dotted", color = "gray") +
+  scale_x_continuous(labels = function(x){format(x, big.mark = ".")}) +
+  scale_color_manual(values = cores, labels = nomes) +
+  labs(y = NULL, color = NULL) +
+  expand_limits(y = 0, x = 0) +
+  theme_bw() +
+  theme(text = element_text(family = "Inter"),
+        panel.grid =element_blank(),
+        panel.border = element_blank(),
+        axis.line.x = element_line(),
+        axis.text.x = element_text(size = 8),
+        axis.ticks.y = element_blank(),
+        legend.position = "top") 
