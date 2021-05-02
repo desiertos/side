4deb46f737630656aba7b004370ed89cab2b720b
tiagombp <tiagombp@gmail.com>
2021-05-01 22:28:57 -0300

    adds bboxes to provinces data

--- prepare2.R
+++ prepare2.R
@@ -7,8 +7,13 @@ library(jsonlite)
 
 dados_geo_depto <- read_sf(dsn = './geo_data/pais_depto', layer = 'pxdptodatosok')
 
+sf::st_crs(dados_geo_depto)
+sf::st_crs(dados_geo_locales)
+
 dados_geo_locales <- read_sf(dsn = './geo_data/pais_locales', layer = 'pxlocdatos')
 
+# teste
+ggplot(dados_geo_locales %>% filter(provincia == "Salta")) + geom_sf() + geom_sf(data = arg_prov)
 
 ## Provincias 
 
@@ -26,13 +31,6 @@ for (i in 1:nrow(arg_prov)) {
     bboxes[i,name] <- bbox[[name]]
   }
 }
-
-arg_prov <- arg_prov %>%
-  left_join(bboxes)
-
-arg_prov_geojson <- geojsonsf::sf_geojson(arg_prov, digits = 6)
-write_file(arg_prov_geojson, "./geo_data/arg_prov.geojson")
-
 
 # fopea data --------------------------------------------------------------
 
@@ -79,8 +77,9 @@ file_provincias <- "./data/provincias.xlsx"
 provincias_raw <- readxl::read_excel(file_provincias)
 
 provincias <- provincias_raw %>%
-  filter(Provincia %in% provincias_disponiveis) #%>%
+  filter(Provincia %in% provincias_disponiveis) %>%
   #left_join(arg_prov, by = c("Provincia" = "nam"))
+  left_join(bboxes, by = c("Provincia" = "nam"))
 
 
 
@@ -108,10 +107,20 @@ lista_locais <- bind_rows(
 
 # output ------------------------------------------------------------------
 
+locales_export <- datos_locales_geom %>% select(-geometry)
+
 output <- list(
-  "localidad" = datos_locales_geom,
+  "localidad" = locales_export,
   "provincia" = provincias,
   "lista_locais" = lista_locais
 )
 
-jsonlite::write_json(output, "output_dash.json")
+jsonlite::write_json(output, "../desiertos.github.io/data/output_dash.json")
+
+arg_prov_geojson <- geojsonsf::sf_geojson(arg_prov, digits = 6)
+write_file(arg_prov_geojson, "../desiertos.github.io/data/maps/arg_prov.geojson")
+
+arg_localidads_geojson <- geojsonsf::sf_geojson(
+  sf::st_as_sf(datos_locales_geom), 
+  digits = 6)
+write_file(arg_localidads_geojson, "../desiertos.github.io/data/maps/arg_localidads.geojson")
