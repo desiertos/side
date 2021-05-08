04c491afa2ce3f9bd5a34d107b475e441cab8f93
tiagombp <tiagombp@gmail.com>
2021-05-08 00:01:47 -0300

    completes adapts_output

--- adapts_output.R
+++ adapts_output.R
@@ -40,13 +40,77 @@ for (i in 1:421) {
 
 municipios_transp <- data.frame(do.call(rbind, muns))
 
-variable_names <- rownames(municipios_transp)
+names_var <- names(mun[["features"]][[1]][["properties"]])
 
-municipios <- municipios_transp %>%
-  data.table::transpose()
+names(municipios_transp) <- names_var
+
+localidad <- municipios_transp %>%
+  rename(local = 1)
+
+
+
+
+# provincia ---------------------------------------------------------------
+
+
+prov <- jsonlite::read_json("../data/output/provincias.json")
+
+provs <- list()
+
+for (i in 1:13) {
   
-names(municipios) <- variable_names
+  df <- prov[["features"]][[i]][["properties"]] %>% 
+    unlist() %>%
+    as.data.frame()
+  
+  variable_names <- rownames(df)
+  df_t <- df %>% data.table::transpose()
 
-muns[[1]] %>% values()
+  names(df_t) <- variable_names
+  
+  provs[[i]] <- df_t
+}
 
-for (i in 1:421) { if (nrow(muns[[i]]) != 11) {print(i)}}
+provincias <- provs %>% bind_rows()
+
+#provs[[1]]
+
+names_var_prov <- names(prov[["features"]][[1]][["properties"]])
+
+names(provincias) <- names_var_prov
+
+provincias_export <- provincias %>%
+  rename(local = 1)
+
+
+# lista locais ------------------------------------------------------------
+
+lista_muns <- localidad %>%
+  mutate(
+    tipo = 'localidad',
+    text = paste0(local, " (", provincia, ")" )
+  ) %>%
+  select(local, tipo, provincia, text)
+
+lista_prov <- provincias_export %>%
+  mutate(
+    tipo = 'provincia',
+    text = paste0(local, " (Provincia)")
+  ) %>%
+  select(local, tipo, text)
+
+locais <- bind_rows(lista_muns, lista_prov)
+
+
+
+# output ------------------------------------------------------------------
+
+output <- list(
+  
+  localidad = localidad,
+  provincia = provincias_export,
+  lista_locais = locais
+  
+)
+
+write_json(output, 'output2.json')
